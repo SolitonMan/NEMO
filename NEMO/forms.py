14e475657faf6f30eaf9df9c583a2b5fe13068a6
SPAAR, DAVID MITCHELL <dms117@psu.edu>
2025-05-01 12:01:31 -0400

    trying to get core to display again

--- NEMO/forms.py
+++ NEMO/forms.py
@@ -239,15 +239,14 @@ class ConsumableOrderForm(forms.ModelForm):
 
 class ConsumableOrderItemForm(forms.ModelForm):
 	search = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder': 'Filter consumables...'}))
-	consumables = ModelChoiceField(queryset=Consumable.objects.filter(category__id=1, visible=True).order_by('name'))
 
 	class Meta:
 		model = ConsumableOrderItem
-		fields = ['search', 'consumables', 'quantity']
+		fields = ['search', 'consumable', 'quantity']
 
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
-		self.fields['consumables'].label_from_instance = lambda obj: f"{obj.name} ({obj.core_id.name if obj.core_id else 'No Core'})"
+		self.fields['consumable'].queryset = Consumable.objects.filter(category__id=1, visible=True).order_by('name')
    
 
 ConsumableOrderItemFormSet = forms.inlineformset_factory(
