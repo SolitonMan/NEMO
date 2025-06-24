ab1276ccf0a9778a3b6750e85e17060ab63d8e31
SPAAR, DAVID MITCHELL <dms117@psu.edu>
2025-06-24 09:34:11 -0400

    updating instructon text for ICS url inclusion

--- NEMO/forms.py
+++ NEMO/forms.py
@@ -348,7 +348,7 @@ class MultiCalendarForm(forms.Form):
 	)
 
 	ics_urls = forms.CharField(
-		widget=forms.Textarea(attrs={'placeholder': 'Enter one ICS URL per line', 'rows': 4,'class': 'form-control'}),
+		widget=forms.Textarea(attrs={'placeholder': 'Enter one ICS URL per line in the format label:url', 'rows': 4,'class': 'form-control'}),
 		label="External Calendar URLs",
 		required=False
 	)
