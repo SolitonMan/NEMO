c0881576e3ee2c04f581a11cb1dcc73b82f4043e
SPAAR, DAVID MITCHELL <dms117@psu.edu>
2026-01-21 12:58:21 -0500

    adding missing references to OuterRef and SubQuery

--- NEMO/views/users.py
+++ NEMO/views/users.py
@@ -8,7 +8,7 @@ from django.conf import settings
 from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
-from django.db.models import Q
+from django.db.models import Q, OuterRef, Subquery
 from django.http import HttpResponseBadRequest, HttpResponseRedirect, JsonResponse, HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
