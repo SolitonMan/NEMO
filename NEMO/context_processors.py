3a3320f87aa91bacb3fe134689765539a2805713
SPAAR, DAVID MITCHELL <dms117@psu.edu>
2026-03-17 16:27:11 -0400

    something still needs the custom_microsoft function

--- NEMO/context_processors.py
+++ NEMO/context_processors.py
@@ -1,15 +1,15 @@
 from django.urls import reverse
 from django.conf import settings
 
-#def custom_microsoft(request):
+def custom_microsoft(request):
 #	"""
 #	Custom context processor to override the Microsoft authorization URL
 #	and always include the desired 'next' parameter.
 #	"""
-#	context = {}
+	context = {}
 #	# Only add if Microsoft login is enabled
-#	microsoft_login_enabled = getattr(settings, "MICROSOFT_AUTH_ENABLED", True)
-#	context["microsoft_login_enabled"] = microsoft_login_enabled
+	microsoft_login_enabled = getattr(settings, "MICROSOFT_AUTH_ENABLED", False)
+	context["microsoft_login_enabled"] = microsoft_login_enabled
 #
 #	if microsoft_login_enabled:
 #		# Always set next to your post-login redirect view
@@ -17,7 +17,7 @@ from django.conf import settings
 #		authorization_url = reverse("microsoft_auth:to-auth-redirect") + f"?next={next_url}"
 #		context["microsoft_authorization_url"] = authorization_url
 #
-#	return context
+	return context
 
 def show_logout_button(request):
 	return {'logout_allowed': True}
